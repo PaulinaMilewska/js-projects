8e8c780681712a78bbb3a7a85a12ecde6859f083
paulina111 <pkozlowska.pw@gmail.com>
2021-04-20 12:31:51 +0200

    remove refactor nav btns, create translate.js

--- flashcards/app.js
+++ flashcards/app.js
@@ -133,78 +133,88 @@ const renderCard = (cards) => {
     let htmlAnswer = "";
     let index = 0;
     for (const [i, value] of Object.entries(cards)) {
-        // console.log(index, value);
         htmlQuestion = `${value.question}`
         htmlAnswer = `${value.answer}`
         index = value.index;
-        // console.log(html);
-        // console.log(index);
     }
     question.innerText = htmlQuestion;
     answer.innerText = htmlAnswer;
     return index;
 };
 
-let index = 0;
+// let index = 0;
 const rightBtn = document.querySelector(".right");
 const leftBtn = document.querySelector(".left");
-let cardIndex = null;
-let prevIndex = 0;
-let maxIndex = 0;
+// let cardIndex = null;
+// let maxIndex = null;
 
-btnNavigationHandler = (btn, indexTrend) => {
-    btn.addEventListener("click", () => {
-        console.log("render: ",renderCard(cardsArray));
-        card.classList.remove("is-flipped");
-        let htmlQuestion = "";
-        let htmlAnswer = "";
-        if(btn === rightBtn){
-            leftBtn.classList.remove("display-none");
-            if(cardIndex !== null) indexTrend = cardIndex + 1;
-            if(indexTrend > maxIndex) indexTrend = 0;
-        } else if(btn === leftBtn){
-            indexTrend = cardIndex - 1;
-            if(indexTrend < 0) indexTrend = maxIndex;
-        }
-        for (const [i, value] of Object.entries(cardsArray)) {
-            index = value.index || 0;
-            htmlQuestion = `${value.question}`;
-            htmlAnswer = `${value.answer}`;
-            if(index === indexTrend){
-                console.log(value);
-                question.innerText = htmlQuestion;
-                answer.innerText = htmlAnswer;
-            }
-        }
-        cardIndex = indexTrend;
-        console.log("cardIndex", cardIndex);
-        if(btn === rightBtn){
-            indexTrend++;
-        }
-    });
-}
 
 let cardsArray = [];
 
 const showCardsByCategory = (category) => getQuestion(category)
 .then( (cardsFromApi) => {
     cardsArray = cardsFromApi;
-    console.log("array", cardsArray);
+    console.log("SHOW THEN 1","array", cardsArray);
 })
 .then(() => {
     renderCard(cardsArray);
-    // console.log("cardsArray", cardsArray);
+    console.log("SHOW THEN 2");
 })
 .then(() => {
+    console.log("SHOW THEN 3");
     card.classList.remove("is-flipped");
+    let index = 0;
+    const rightBtn = document.querySelector(".right");
+    const leftBtn = document.querySelector(".left");
     leftBtn.classList.add("display-none");
-    rightBtn.classList.remove("display-none");
-    maxIndex = renderCard(cardsArray);
+    const maxIndex = renderCard(cardsArray);
+    let cardIndex = null;
     let nextIndex = renderCard(cardsArray) + 1;
+    let prevIndex = 0;
+    rightBtn.classList.remove("display-none");
     console.log("before click index", index);
     console.log("before click nextIndex", nextIndex);
-    btnNavigationHandler(rightBtn, nextIndex);
-    btnNavigationHandler(leftBtn, prevIndex);
+    rightBtn.addEventListener("click", () => {
+        console.log("render: ",renderCard(cardsArray));
+        leftBtn.classList.remove("display-none");
+        card.classList.remove("is-flipped");
+        let htmlQuestion = "";
+        let htmlAnswer = "";
+        if(cardIndex !== null) nextIndex = cardIndex + 1;
+        if(nextIndex > maxIndex) nextIndex = 0;
+        for (const [i, value] of Object.entries(cardsArray)) {
+            index = value.index || 0;
+            htmlQuestion = `${value.question}`;
+            htmlAnswer = `${value.answer}`;
+            if(index === nextIndex){
+                console.log(value);
+                question.innerText = htmlQuestion;
+                answer.innerText = htmlAnswer;
+            }
+        }
+        cardIndex = nextIndex;
+        console.log("cardIndex", cardIndex);
+        nextIndex++;
+    });
+    leftBtn.addEventListener("click", () => {
+        card.classList.remove("is-flipped");
+        prevIndex = cardIndex - 1;
+        if(prevIndex < 0) prevIndex = maxIndex;
+        let htmlQuestion = "";
+        let htmlAnswer = "";
+        for (const [i, value] of Object.entries(cardsArray)) {
+            index = value.index || 0;
+            htmlQuestion = `${value.question}`;
+            htmlAnswer = `${value.answer}`;
+            if(index === prevIndex){
+                console.log(value);
+                question.innerText = htmlQuestion;
+                answer.innerText = htmlAnswer;
+            }
+        }
+        cardIndex = prevIndex;
+        console.log("cardIndex", cardIndex);
+    });
 });
 
 const home = document.querySelector(".home");
@@ -230,20 +240,17 @@ const react = document.querySelector(".react");
 const reactLink = document.querySelector(".react a");
 const links = document.querySelectorAll(".navigation ul li a");
 
-categoryBtnHandler = (categoryBtn, category) => {
+categoryBtnHandler = (categoryBtn, categoryLink, category) => {
     categoryBtn.addEventListener('click', () => {
         links.forEach((el) => {
             el.classList.remove("clicked");
           });
-        htmlLink.classList.add("clicked");
+          categoryLink.classList.add("clicked");
+        // maxIndex = renderCard(cardsArray);
         showCardsByCategory(category);
     });
 }
-categoryBtnHandler(html, "html");
-categoryBtnHandler(css, "css");
-categoryBtnHandler(js, "js");
-categoryBtnHandler(react, "react");
-
-function googleTranslateElementInit() {
-    new google.translate.TranslateElement({pageLanguage: 'en'}, 'google_translate_element');
-  }
+categoryBtnHandler(html, htmlLink, "html");
+categoryBtnHandler(css, cssLink, "css");
+categoryBtnHandler(js, jsLink, "js");
+categoryBtnHandler(react, reactLink, "react");
