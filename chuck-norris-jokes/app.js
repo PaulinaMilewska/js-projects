fb8ae8326ea98cdb070e1a5f0f2251c6b0d5d8b2
paulina111 <pkozlowska.pw@gmail.com>
2021-05-06 19:58:17 +0200

    add category menu, change prettier settings

--- chuck-norris-jokes/app.js
+++ chuck-norris-jokes/app.js
@@ -1,49 +1,55 @@
-const joke = document.querySelector(".joke");
+const joke = document.querySelector('.joke');
 
 function getRandomJoke() {
-    return fetch("https://api.chucknorris.io/jokes/random")
-    .then(res => res.json())
-    .then(data => data.value)
-    .catch(rej => console.log(rej.message));
+    return fetch('https://api.chucknorris.io/jokes/random')
+        .then((res) => res.json())
+        .then((data) => data.value)
+        .catch((rej) => console.log(rej.message));
 }
-const chuckBtn = document.querySelector(".box a");
+const chuckBtn = document.querySelector('.box a');
 
 function renderJoke(jokeText) {
-    const getJoke =  `${jokeText}`;
+    const getJoke = `${jokeText}`;
     joke.innerHTML = getJoke;
 }
 
 // getRandomJoke();
-let jokeContent = "";
+let jokeContent = '';
 
 const printJoke = () => {
     getRandomJoke()
-    .then((jokeApi) => {
-        jokeContent = jokeApi;
-    })
-    .then(() => {
-        renderJoke(jokeContent)
-    })
-}
+        .then((jokeApi) => {
+            jokeContent = jokeApi;
+        })
+        .then(() => {
+            renderJoke(jokeContent);
+        });
+};
 
-function getCategories(){
-    fetch("https://api.chucknorris.io/jokes/categories")
-    .then(res => res.json())
-    .then(data => console.log(data))
-    .catch(rej => console.log(rej.message));
+function getCategories() {
+    fetch('https://api.chucknorris.io/jokes/categories')
+        .then((res) => res.json())
+        .then((data) => console.log(data))
+        .catch((rej) => console.log(rej.message));
 }
 
 // getCategories();
 
-function getJokesByQuery(query){
+function getJokesByQuery(query) {
     fetch(`https://api.chucknorris.io/jokes/search?query=${query}`)
-    .then(res => res.json())
-    .then(data => console.log(data))
-    .catch(rej => console.log(rej.message));
+        .then((res) => res.json())
+        .then((data) => console.log(data))
+        .catch((rej) => console.log(rej.message));
 }
 
 // getJokesByQuery("dog");
 
-
-
 chuckBtn.addEventListener('click', printJoke());
+
+const hiddenInfo = document.querySelectorAll('[data-hidden]');
+
+hiddenInfo.forEach((info, index) => {
+    info.classList.add('hidden');
+});
+
+const categories = document.querySelectorAll('.categories li');
